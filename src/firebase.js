789875fbb4f437b412c50011456894e91dd1f6c1
Jayadurga M S <85250733+jayadurga-ms-208@users.noreply.github.com>
2024-01-19 07:56:51 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,7 @@ import { getFirestore } from "firebase/firestore";
 import  { getAuth, GoogleAuthProvider } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCcY_KguBKJiv8Gh5zAZYjon19khE7kmFg",
-  authDomain: "talksapp-ec39f.firebaseapp.com",
-  databaseURL: "https://talksapp-ec39f-default-rtdb.firebaseio.com",
-  projectId: "talksapp-ec39f",
-  storageBucket: "talksapp-ec39f.appspot.com",
-  messagingSenderId: "593017204235",
-  appId: "1:593017204235:web:8e897fb66fa90a3a88210e",
-  measurementId: "G-6HVZQ5SWD2"
+  // APIKEY
   };
 
   const firebaseApp = initializeApp(firebaseConfig);
